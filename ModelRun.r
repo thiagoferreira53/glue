48442e0a91a4f7126c8c44234d3b4e3e4deae380
Thiago Berton Ferreira <thiago.bertonf@gmail.com>
2024-03-22 11:57:12 -0400

    Writing calibration progress on "progress.txt" file

--- ModelRun.r
+++ ModelRun.r
@@ -2,6 +2,111 @@ library(parallel)
 
 progress <- TRUE
 
+# Custom version of pbapply (library pbapply) for writing the calibration status on "progess.txt"
+pbapply_custom <- function (X, FUN, ..., cl = NULL) 
+{
+  FUN <- match.fun(FUN)
+  if (!is.vector(X) || is.object(X)) 
+    X <- as.list(X)
+  if (!length(X)) 
+    return(lapply(X, FUN, ...))
+  if (!is.null(cl)) {
+    if (identical(cl, "future")) {
+      if (!requireNamespace("future") || !requireNamespace("future.apply")) {
+        warning("You need some packages for cl='future' to work: install.packages('future.apply')")
+        cl <- NULL
+      }
+    }
+    else {
+      if (.Platform$OS.type == "windows") {
+        if (!inherits(cl, "cluster")) 
+          cl <- NULL
+      }
+      
+      else {
+        if (inherits(cl, "cluster")) {
+          if (length(cl) < 2L) 
+            cl <- NULL
+        }
+        else {
+          if (cl < 2) 
+            cl <- NULL
+        }
+      }
+    }
+  }
+  nout <- as.integer(getOption("pboptions")$nout)
+  if (is.null(cl)) {
+    if (!dopb()) 
+      return(lapply(X, FUN, ...))
+    Split <- splitpb(length(X), 1L, nout = nout)
+    B <- length(Split)
+    pb <- startpb(0, B)
+    on.exit(closepb(pb), add = TRUE)
+    rval <- vector("list", B)
+    for (i in seq_len(B)) {
+      rval[i] <- list(lapply(X[Split[[i]]], FUN, ...))
+      setpb(pb, i)
+    }
+  }
+  else {
+    if (inherits(cl, "cluster")) {
+      PAR_FUN <- if (isTRUE(getOption("pboptions")$use_lb)) 
+        parallel::parLapplyLB
+      else parallel::parLapply
+      if (!dopb()) 
+        return(PAR_FUN(cl, X, FUN, ...))
+      Split <- splitpb(length(X), length(cl), nout = nout)
+      B <- length(Split)
+      pb <- startpb(0, B)
+      on.exit(closepb(pb), add = TRUE)
+      rval <- vector("list", B)
+      for (i in seq_len(B)) {
+        rval[i] <- list(PAR_FUN(cl, X[Split[[i]]], FUN, 
+                                ...))
+        setpb(pb, i)
+        write.table(i/B*100,file = paste0(OD,'process.txt'), row.names = FALSE, col.names = FALSE)
+      }
+    }
+    else if (identical(cl, "future")) {
+      requireNamespace("future")
+      requireNamespace("future.apply")
+      if (!dopb()) 
+        return(future.apply::future_lapply(X, FUN, ..., 
+                                           future.stdout = FALSE))
+      Split <- splitpb(length(X), future::nbrOfWorkers(), 
+                       nout = nout)
+      B <- length(Split)
+      pb <- startpb(0, B)
+      on.exit(closepb(pb), add = TRUE)
+      rval <- vector("list", B)
+      for (i in seq_len(B)) {
+        rval[i] <- list(future.apply::future_lapply(X[Split[[i]]], 
+                                                    FUN, ..., future.stdout = FALSE))
+        setpb(pb, i)
+      }
+    }
+    else {
+      if (!dopb()) 
+        return(parallel::mclapply(X, FUN, ..., mc.cores = as.integer(cl), 
+                                  mc.silent = TRUE))
+      Split <- splitpb(length(X), as.integer(cl), nout = nout)
+      B <- length(Split)
+      pb <- startpb(0, B)
+      on.exit(closepb(pb), add = TRUE)
+      rval <- vector("list", B)
+      for (i in seq_len(B)) {
+        rval[i] <- list(parallel::mclapply(X[Split[[i]]], 
+                                           FUN, ..., mc.cores = as.integer(cl), mc.silent = TRUE))
+        setpb(pb, i)
+      }
+    }
+  }
+  rval <- do.call(c, rval, quote = TRUE)
+  names(rval) <- names(X)
+  rval
+}
+
 ##This is the function to run the DSSAT model.
 ModelRun<-function(WD, OD, DSSATD, GD, CropName, GenotypeFileName, CultivarID, RoundOfGLUE, TotalParameterNumber, 
                    NumberOfModelRun, RandomMatrix, CoresAvailable, EcotypeID, EcotypeParameters, ModelSelect, CTR)
@@ -123,7 +228,8 @@ ModelRun<-function(WD, OD, DSSATD, GD, CropName, GenotypeFileName, CultivarID, R
       library(pbapply)
       
       pbo = pboptions(type="txt")
-      system.time(pblapply(ListModelRun, function(z) run_simulations(z), cl = cl))
+      #pblapply(ListModelRun, function(z) run_simulations(z), cl = cl)
+      pbapply_custom(ListModelRun, function(z) run_simulations(z), cl = cl)
     }else{
       parLapply(NULL, ListModelRun, function(z) run_simulations(z))
     }
